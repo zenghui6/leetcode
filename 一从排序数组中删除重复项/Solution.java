1a42790eb8c87c6c06e7802b57b25f16f9cd77e0
zenghui6 <38882195+zenghui6@users.noreply.github.com>
2019-03-17 19:40:33 +0800

    尝试
    为什么不能上传文件夹

---
+++ 一从排序数组中删除重复项/Solution.java
@@ -0,0 +1,16 @@
+package 一从排序数组中删除重复项;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length==0) return 0;
+        else{
+            int i = 0,j;
+            for(j=1;j<nums.length;j++){
+                if (nums[j] != nums[i]) {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i+1;
+        }
+    }}
