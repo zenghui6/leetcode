70459a1454c31d8eb25f4416236e81827b255a0e
zenghui6 <1210128434@qq.com>
2020-03-20 21:11:38 +0800

    3月20

---
+++ LeetCode/p40最小k个数/Solution.java
@@ -0,0 +1,34 @@
+package LeetCode.p40最小k个数;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName Solution
+ * @Description: 写排序就行,但是排序已经写烂了,直接写吧
+ * @Author zeng
+ * @Date 2020/3/20
+ **/
+public class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 统计每个数字出现的次数
+        int[] counter = new int[10001];
+        for (int num: arr) {
+            counter[num]++;
+        }
+        // 根据counter数组从头找出k个数作为返回结果
+        int[] res = new int[k];
+        int idx = 0;
+        for (int num = 0; num < counter.length; num++) {
+            while (counter[num]-- > 0 && idx < k) {
+                res[idx++] = num;
+            }
+            if (idx == k) {
+                break;
+            }
+        }
+        return res;
+    }
+}
